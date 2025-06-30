47e63ae8769e948e67c955bf9ee4383570cc2e84
Tyler Wu <tylerwu2222@gmail.com>
2025-06-29 17:17:07 -0700

    [0.1] clean up types + components

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import { FluoresceNode } from "../types";
+import { Node } from "../types";
 import Node from "../Node";
 import { exampleNodes } from "../../examples/example_nodes";
 
 export const Editor = () => {
 
   // state representing blocks
-  const [nodes, setNodes] = useState<FluoresceNode[]>(exampleNodes)
+  const [nodes, setNodes] = useState<Node[]>(exampleNodes)
 
   return <div>
     {nodes.map(node => (
--- src/components/types.ts
+++ src/components/types.ts
@@ -1,5 +1,5 @@
 // types for block and inline nodes
-type HeadingType =
+type Heading =
   | "heading1"
   | "heading2"
   | "heading3"
@@ -7,13 +7,15 @@ type HeadingType =
   | "heading5"
   | "heading6";
 
-export type FluoresceBlockType =
+export type HeadingTag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
+export type Block =
   | "paragraph"
-  | HeadingType
+  | Heading
   | "list"
   | "listItem";
 
-export type FluoresceInlineType =
+export type Inline =
   | "bold"
   | "italic"
   | "underline"
@@ -21,47 +23,45 @@ export type FluoresceInlineType =
   | "link";
 
 // base node to extend to specific node types
-interface BaseNode {
+export interface Node {
   id: string;
-  type: FluoresceBlockType | FluoresceInlineType;
-  content?: BaseNode[]; // for inline content
-  items?: BaseNode[]; // for list type items
+  type: Block | Inline;
+  content?: Node[]; // for inline content
+  items?: Node[]; // for list type items
   text?: string; // for text based nodes
   props?: Record<string, any>;
   styles?: Record<string, any>;
 }
 
 // specific node types
-export interface ParagraphNode extends BaseNode {
+export interface ParagraphNode extends Node {
   type: "paragraph";
   text: string;
 }
 
-export interface HeadingNode extends BaseNode {
-  type: HeadingType;
+export interface HeadingNode extends Node {
+  type: Heading;
   text: string;
 }
 
-export interface ListNode extends BaseNode {
+export interface ListNode extends Node {
   type: "list";
   items: ListItemNode[];
 }
 
-export interface ListItemNode extends BaseNode {
+export interface ListItemNode extends Node {
   type: "listItem";
   text: string;
 }
 
 // union node types
-export interface BlockNode extends BaseNode {
-  type: FluoresceBlockType;
+export interface BlockNode extends Node {
+  type: Block;
   content?: InlineNode[] | BlockNode[]; // depending on block type
   
 }
 
-export interface InlineNode extends BaseNode {
-  type: FluoresceInlineType;
+export interface InlineNode extends Node {
+  type: Inline;
   text: string;
 }
-
-export type FluoresceNode = BlockNode | InlineNode;
