bad1d85afb93de814fa2b61c77e024d15ca2259a
Tyler Wu <tylerwu2222@gmail.com>
2025-07-06 14:21:41 -0700

    set up plain text plugin

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,8 +1,32 @@
-// src/Editor.tsx
+// src/components/Editor.tsx
+import {
+  LexicalComposer
+} from "@lexical/react/LexicalComposer";
+import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
 import { jsx } from "react/jsx-runtime";
-var Editor = () => {
-  return /* @__PURE__ */ jsx("div", { contentEditable: true, children: "Start typing here..." });
+var theme = {
+  // Customize styling or leave empty to use default
 };
+function Editor() {
+  const initialConfig = {
+    namespace: "FluoresceEditor",
+    theme,
+    onError(error) {
+      throw error;
+    },
+    editorState: null
+  };
+  return /* @__PURE__ */ jsx(LexicalComposer, { initialConfig, children: /* @__PURE__ */ jsx(
+    PlainTextPlugin,
+    {
+      contentEditable: /* @__PURE__ */ jsx(ContentEditable, { className: "editor-input" }),
+      placeholder: /* @__PURE__ */ jsx("div", { className: "editor-placeholder", children: "Type lab notes\u2026" }),
+      ErrorBoundary: LexicalErrorBoundary
+    }
+  ) });
+}
 export {
   Editor
 };
